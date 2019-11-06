a7adc14b8e31de9bde9f2dc9f6741c4ba3643376
logarithm <iamrohan04@gmail.com>
2019-11-06 23:49:48 +0530

    added admin dashboard

---
+++ app/src/main/java/com/logarithm/airticket/flightticketbook/AdminDashboard.java
@@ -0,0 +1,16 @@
+package com.logarithm.airticket.flightticketbook;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.logarithm.airticket.flightticketbook.R;
+
+public class AdminDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+    }
+}
