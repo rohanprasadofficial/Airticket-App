b0fb0496915eda3ab09a9180489900e29be0ba65
logarithm <iamrohan04@gmail.com>
2019-11-07 23:53:33 +0530

    added delete flight

--- app/src/main/java/com/logarithm/airticket/flightticketbook/DeleteFlight.java
+++ app/src/main/java/com/logarithm/airticket/flightticketbook/DeleteFlight.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.logarithm.airticket.flightticketbook.ModelClass.DeleteFlight.Message;
 import com.logarithm.airticket.flightticketbook.RestAPI.APIClient;
 import com.logarithm.airticket.flightticketbook.RestAPI.APIInterface;
+import com.logarithm.airticket.flightticketbook.utils.MyRecyclerItemClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,57 +39,56 @@ public class DeleteFlight extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         // set a LinearLayoutManager with default vertical orientaion
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(linearLayoutManager); // set LayoutManager to RecyclerView
+        recyclerView.setLayoutManager(linearLayoutManager); // set LayoutManager to RecyclerVie
 
-//
-//        recyclerView.addOnItemTouchListener(
-//                new MyRecyclerItemClickListener(getApplicationContext(), new MyRecyclerItemClickListener.OnItemClickListener() {
-//                    @Override
-//                    public void onItemClick(View view, final int position) {
-//
-//                        alertDialog = new SpotsDialog.Builder().setContext(Trips.this).setTheme(R.style.Custom).build();
-//                        alertDialog.setMessage("Verifying passport.. ");
-//                        alertDialog.show();
-//
-//                        final APIInterface apiService = APIClient.getClient().create(APIInterface.class);
-//                        InitiatorAppData initiatorAppData=new InitiatorAppData(tripList.get(position).getTripId());
-//                        List<Trigger> trigger=new ArrayList<>();
-//                        trigger.add(new Trigger("planATrip",initiatorAppData,"airportSecurity"));
-//                        PassportPermission passportPermission=new PassportPermission(SCANNED_TEXT,trigger);
-//                        Call<Permission> call2 = apiService.verifyPassport(passportPermission,"Bearer "+ MainActivity.TOKEN_ID);
-//                        call2.enqueue(new Callback<Permission>() {
-//                            @Override
-//                            public void onResponse(Call<Permission> call, Response<Permission> response) {
-//                                try {
-//                                    alertDialog.dismiss();
-//                                    Log.i("JSON", response.body().getSuccess().toString());
-//                                    if (response.body().getSuccess()) {
-//                                        Toast.makeText(Trips.this, "Passport verified successfully!", Toast.LENGTH_SHORT).show();
-//                                        finish();
-//                                    } else {
-//                                        Toast.makeText(Trips.this, response.body().getError(), Toast.LENGTH_SHORT).show();
-//                                    }
-//
-//                                } catch (Exception e) {
-//                                    alertDialog.dismiss();
-//                                    Toast.makeText(Trips.this, "Something Went Wrong-1 ", Toast.LENGTH_SHORT).show();
-//                                    e.printStackTrace();
-//
-//                                }
-//                            }
-//
-//                            @Override
-//                            public void onFailure(Call<Permission> call, Throwable t) {
-//                                alertDialog.dismiss();
-//                                t.printStackTrace();
-//                                Toast.makeText(Trips.this, "Something Went Wrong -2", Toast.LENGTH_SHORT).show();
-//                            }
-//                        });
-//
-//
-//                    }
-//                })
-//        );
+
+
+        recyclerView.addOnItemTouchListener(
+                new MyRecyclerItemClickListener(getApplicationContext(), new MyRecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, final int position) {
+
+                        alertDialog = new SpotsDialog.Builder().setContext(DeleteFlight.this).setTheme(R.style.Custom).build();
+                        alertDialog.setMessage("Deleting Flight.. ");
+                        alertDialog.show();
+
+                        final APIInterface apiService = APIClient.getClient().create(APIInterface.class);
+
+                        PassportPermission passportPermission=new PassportPermission(SCANNED_TEXT,trigger);
+                        Call<Permission> call2 = apiService.verifyPassport(passportPermission,"Bearer "+ MainActivity.TOKEN_ID);
+                        call2.enqueue(new Callback<Permission>() {
+                            @Override
+                            public void onResponse(Call<Permission> call, Response<Permission> response) {
+                                try {
+                                    alertDialog.dismiss();
+                                    Log.i("JSON", response.body().getSuccess().toString());
+                                    if (response.body().getSuccess()) {
+                                        Toast.makeText(Trips.this, "Passport verified successfully!", Toast.LENGTH_SHORT).show();
+                                        finish();
+                                    } else {
+                                        Toast.makeText(Trips.this, response.body().getError(), Toast.LENGTH_SHORT).show();
+                                    }
+
+                                } catch (Exception e) {
+                                    alertDialog.dismiss();
+                                    Toast.makeText(Trips.this, "Something Went Wrong-1 ", Toast.LENGTH_SHORT).show();
+                                    e.printStackTrace();
+
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<Permission> call, Throwable t) {
+                                alertDialog.dismiss();
+                                t.printStackTrace();
+                                Toast.makeText(Trips.this, "Something Went Wrong -2", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+                    }
+                })
+        );
 
         // call the constructor of CustomAdapter to send the reference and data to Adapter
         alertDialog = new SpotsDialog.Builder().setContext(DeleteFlight.this).setTheme(R.style.Custom).build();
