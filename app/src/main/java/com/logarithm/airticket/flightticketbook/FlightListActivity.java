d0a9dcf2890869a7e0f3f9210e1af1eaa6750296
logarithm <iamrohan04@gmail.com>
2019-11-09 16:43:26 +0530

    get flights in flightlist

--- app/src/main/java/com/logarithm/airticket/flightticketbook/FlightListActivity.java
+++ app/src/main/java/com/logarithm/airticket/flightticketbook/FlightListActivity.java
@@ -1,15 +1,30 @@
 package com.logarithm.airticket.flightticketbook;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.logarithm.airticket.flightticketbook.Adapter.RecyclerAdapter_Flight;
+import com.logarithm.airticket.flightticketbook.ModelClass.DeleteFlight.Message;
 import com.logarithm.airticket.flightticketbook.ModelClass.Flight;
+import com.logarithm.airticket.flightticketbook.RestAPI.APIClient;
+import com.logarithm.airticket.flightticketbook.RestAPI.APIInterface;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.logarithm.airticket.flightticketbook.LoginAdmin.TOKEN_ID_ADMIN;
 
 public class FlightListActivity extends AppCompatActivity {
 
@@ -17,8 +32,14 @@ public class FlightListActivity extends AppCompatActivity {
 
 //    RecyclerView recyclerView;
 //    FlightAdapter adapter;
-//
 //    List<Flight>productlist;
+
+
+
+    public String Source,Destination;
+    public AlertDialog alertDialog = null;
+    List<Message> tripList;
+
 
     private String txtFlightName  []= {"AIRFRANCE ","Emirates","AIRFRANCE"};
     private String txtCountry []={"Paris","Paris","AIRFRANCE"};
@@ -38,11 +59,61 @@ public class FlightListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flight_list_view);
+        Source=getIntent().getStringExtra("FROM");
+        Destination=getIntent().getStringExtra("TO");
 
+        try {
+            // call the constructor of CustomAdapter to send the reference and data to Adapter
+            alertDialog = new SpotsDialog.Builder().setContext(FlightListActivity.this).setTheme(R.style.Custom).build();
+            alertDialog.setMessage("Getting flights info..");
+            alertDialog.show();
+            final APIInterface apiService = APIClient.getClient().create(APIInterface.class);
+            Call<com.logarithm.airticket.flightticketbook.ModelClass.DeleteFlight.DeleteFlight> call2 = apiService.getAllflights(TOKEN_ID_ADMIN);
+            call2.enqueue(new Callback<com.logarithm.airticket.flightticketbook.ModelClass.DeleteFlight.DeleteFlight>() {
+                @Override
+
+                public void onResponse(Call<com.logarithm.airticket.flightticketbook.ModelClass.DeleteFlight.DeleteFlight> call, Response<com.logarithm.airticket.flightticketbook.ModelClass.DeleteFlight.DeleteFlight> response) {
+                    try {
+                        alertDialog.dismiss();
+                        Log.i("JSON", response.body().getSuccess().toString());
+                        if (response.body().getSuccess()) {
+                            alertDialog.dismiss();
+                            tripList = response.body().getMessage();
+                            if (tripList.size() == 0) {
+                                Toast.makeText(FlightListActivity.this, "No Flights Available !", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(FlightListActivity.this, tripList.get(0).getName(), Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        } else {
+                            Log.i("TEST", response.body().getMessage().toString());
+                        }
+                    } catch (Exception e) {
+                        alertDialog.dismiss();
+                        Toast.makeText(FlightListActivity.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<com.logarithm.airticket.flightticketbook.ModelClass.DeleteFlight.DeleteFlight> call, Throwable t) {
+                    alertDialog.dismiss();
+                    Toast.makeText(FlightListActivity.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
 
 
         flights = new ArrayList<>();
-
         recyclerView=(RecyclerView)findViewById(R.id.recyclerview);
 
 
@@ -51,8 +122,6 @@ public class FlightListActivity extends AppCompatActivity {
                     txtCountryDestination[i],txtStationDestination[i],btnBuy[i]);
             flights.add(beanClassForRecyclerView_contacts);
         }
-
-
 
 
         mAdapter = new RecyclerAdapter_Flight(FlightListActivity.this,flights);
